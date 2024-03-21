41c262e98d9013d865aa1e448f59b60cbb7f3e4b
Luis Santiago <luissantiago@Luiss-Air.lan>
2024-03-21 14:49:43 -0400

    Passed props to Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,7 +10,7 @@ function Post(){
     }
     return(
         <div>
-            <h3>Test Post</h3>
+            <h3>{props.content}</h3>
             <p>This is a test post we'll edit later!</p>
             <p>Likes: {likes}</p>
             <button onClick={handleLike}>Like</button>
